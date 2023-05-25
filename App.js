787c74e7501a83d6ae309cd1e8eb7c82e208ff85
Paulo Alceu <122623481+pauloalceu@users.noreply.github.com>
2023-05-25 15:07:20 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import './App.css';
+import SeuNome from './SeuNome';
+import Saudacao from "./Saudacao";
+
+function App() {
+
+    const [nome, setNome] = useState()
+
+    return (
+    <div className="App">
+      <h1> State Lift</h1>
+      <SeuNome  setNome={setNome}/>
+      <Saudacao nome={nome} />
+    </div>
+  )
+}
+
+export default App
